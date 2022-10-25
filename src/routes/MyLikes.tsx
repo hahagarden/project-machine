3eda1563a9ca802243bf250a8f6717a3166f03fe
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-25 17:31:07 +0900

    feat: customize mylikes category

--- src/routes/MyLikes.tsx
+++ src/routes/MyLikes.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 import MyLike from "./components_mylikes/MyLike";
 import { mylikesCategoryAtom } from "./components_mylikes/atoms_mylikes";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { dbService } from "../fbase";
+import { setDoc, doc, onSnapshot } from "firebase/firestore";
+import { loggedInUserAtom } from "../atom";
 
 const Wrapper = styled.div`
   display: flex;
@@ -47,22 +52,85 @@ const Button = styled.button`
   }
 `;
 
+interface ItempForm {
+  categoryName: string;
+  inputInfos: string;
+  radioInfo: string;
+  radioInfos: string;
+}
+
 function MyLikes() {
   const setMyLikesCategory = useSetRecoilState(mylikesCategoryAtom);
   const navigate = useNavigate();
   const onClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const currentCategory = event.currentTarget.innerText;
-    setMyLikesCategory(currentCategory + "s");
+    setMyLikesCategory(currentCategory);
     navigate(`/mylikes/${currentCategory}/table`);
   };
+
+  const [myLikesTemplate, setMyLikesTemplate] = useState({});
+  const loggedInUser = useRecoilValue(loggedInUserAtom);
+  const { register, handleSubmit } = useForm<ItempForm>();
+  const [addTempCategory, setAddTempCategory] = useState(false);
+  const tempClick = () => {
+    setAddTempCategory((prev) => !prev);
+  };
+  const tempSubmit = async (data: ItempForm) => {
+    const categoryTemplate = {
+      inputInfos: data.inputInfos.replace(/ /g, ""),
+      radioInfo: data.radioInfo.replace(/ /g, ""),
+      raedioInfos: data.radioInfos.replace(/ /g, ""),
+    };
+    try {
+      await setDoc(
+        doc(dbService, "MyLikes_template", `template_${loggedInUser?.uid}`),
+        { [data.categoryName]: categoryTemplate },
+        { merge: true }
+      ); //add ranking_uid document
+    } catch (e) {
+      console.error("Error adding document", e);
+    }
+  };
+  useEffect(() => {
+    onSnapshot(
+      doc(dbService, "MyLikes_template", `template_${loggedInUser?.uid}`),
+      (doc) => {
+        let templateDB = {};
+        templateDB = { ...templateDB, ...doc.data() };
+        setMyLikesTemplate(templateDB);
+      }
+    );
+    console.log("mylikes_template set.");
+  }, []);
+  console.log("template----", myLikesTemplate);
+
   return (
     <>
       <Wrapper>
         <Header>
-          <Title>My Likes</Title>
+          <Title>
+            My Likes
+            <button onClick={tempClick}>temp +</button>
+            {addTempCategory ? (
+              <form onSubmit={handleSubmit(tempSubmit)}>
+                <label htmlFor="categoryName">category Name</label>
+                <input id="categoryName" {...register("categoryName")}></input>
+                <label htmlFor="inputInfos">input infos</label>
+                <input id="inputInfos" {...register("inputInfos")}></input>
+                <label htmlFor="radioInfo">radio infos</label>
+                <input id="radioInfo" {...register("radioInfo")}></input>
+                <label htmlFor="radioInfos">radio infos</label>
+                <input id="radioInfos" {...register("radioInfos")}></input>
+                <button>submit</button>
+              </form>
+            ) : null}
+          </Title>
           <Menu>
-            <Button onClick={onClick}>song</Button>
-            <Button onClick={onClick}>movie</Button>
+            {Object.keys(myLikesTemplate).map((category) => (
+              <Button key={category} onClick={onClick}>
+                {category}
+              </Button>
+            ))}
           </Menu>
         </Header>
         <Routes>
